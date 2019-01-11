91f494f26efd559daf8015cb66acba175b0234c0
rooty <rooty@fake.com>
2019-01-11 13:56:01 -0600

    more mongo tools

--- emaconfigs.js
+++ emaconfigs.js
@@ -1,19 +1,32 @@
-var proj  = project('fast_flash_card');
-function ids(coll, query) { return coll.find(query).map(it=>it._id) };
-var instruments = ids(db.instrument, {project: proj._id});
-function badTrigX(coll) {
-    var configs = ids(db.instrumentConfig, {instrument: {$in: instruments}});
-    var surveys = ids(db.emaOtsCardstack, {configuration: {$in: configs}})
-    var triggers = ids(db.trigger, {configuration: {$in: configs}})
-    coll.find({trigger: {$in: triggers}, survey: {$exists: 1, $nin: surveys}})
-        .forEach(te => 
-            db.trigger.find({_id: te.trigger})
-            .forEach(trig =>
-                db.instrumentConfig.find({_id: trig.configuration})
-                .forEach(config =>
-                    printjson([config.code, trig.code, te])
-        )));
+print('*********** emaconfig.js')
+
+/*
+ * find and optianlly fix dangling survey references
+ */
+function badTrigs(projCode, fixIt) {
+    var proj = db.project.findOne({code: projCode})
+    db.instrument.find({project: proj._id}).forEach(instrument => { 
+        db.instrumentConfig.find({instrument: {$in: instruments}}).forEach(config => {
+            var surveys = ids(db.emaOtsCardstack, {configuration: config._id})
+            var triggers = ids(db.trigger, {configuration: config._id})
+            db.triggerAction.find({ trigger: { $in: triggers },
+                                    survey: { $exists: 1, $nin: surveys }})
+                .forEach(badAction => {
+                    var trigCode = db.trigger.findOne({_id: badAction.trigger}).code
+                    printjson([config.code, trigCode, badAction.survey, badAction])
+                    if (fixIt) {
+                        var result = db.triggerAction.update(
+                            {_id: badAction._id},
+                            {$set: {_bad_survey: badAction.survey, survey: surveys[0]}})
+                        printjson([ '==>', surveys[0], result ])
+                    }
+
+            })
+        })
+    })
 }
+
+
 //badTrigX(db.triggerEvent)
 //db.triggerEvent.remove({_id: {$in: ['5abbe369e4b0c24002e3dc7d', '5abbe5c4e4b0c24002e3dcbe']}})
 //db.triggerAction.find({trigger: {$in: triggers}, survey: {$exists: 1, $nin: surveys}})
