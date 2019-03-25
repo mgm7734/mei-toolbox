8c6f4d8bb77da7b05bf4a2c4eca59a245fefb374
rooty <rooty@fake.com>
2019-03-25 14:49:29 -0500

    dump-csv

---
+++ dump-csv.js
@@ -0,0 +1,93 @@
+// doesn't flatten
+function printCSV(cursor) {
+    var headers = []
+
+
+    while(cursor.hasNext()) {
+        var flatRow = {};
+        eachFlattenedProperty(cursor.next(), (key, value) => {
+            flatRow[key] = value;
+            if (headers.indexOf(key) == -1) {
+                headers.push(key)
+            }
+        })
+        var columns = headers.map(flatKey => flatRow[flatKey]).map(quoteValue)
+        print(columns.join(','))
+    }
+    print(headers.join(','))
+}
+
+function quoteValue(value) {
+    if (typeof value != 'string') return value;
+    var escaped = value.split('"').join('""')
+    return `"${escaped}"`
+}
+
+function eachFlattenedProperty(obj, fn, opt = {prefix: '', pathSep: '.' }) {
+    Object.getOwnPropertyNames(obj).forEach(nm => {
+        var fullName = opt.prefix + nm;
+        var val = obj[nm];
+        if (typeof val == 'object') {
+            eachFlattenedProperty(val, fn, { prefix: fullName + opt.pathSep, pathSep: opt.pathSep })
+        }
+        else {
+            fn(fullName, val)
+        }
+    })
+}
+
+/*
+DBQuery.prototype.toCSV = function(deliminator, textQualifier)
+{
+    var count = -1;
+    var headers = [];
+    var data = {};
+
+    var cursor = this;
+
+    deliminator = deliminator == null ? ',' : deliminator;
+    textQualifier = textQualifier == null ? '\"' : textQualifier;
+
+    while (cursor.hasNext()) {
+
+        var array = new Array(cursor.next());
+
+        count++;
+
+        for (var index in array[0]) {
+            if (headers.indexOf(index) == -1) {
+                headers.push(index);
+            }
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            for (var index in array[i]) {
+                data[count + '_' + index] = array[i][index];
+            }
+        }
+    }
+
+    var line = '';
+
+    for (var index in headers) {
+        line += headers[index] + ',';
+    }
+
+    line = line.slice(0, -1);
+    print(line);
+
+    for (var i = 0; i < count + 1; i++) {
+
+        var line = '';
+        var cell = '';
+        for (var j = 0; j < headers.length; j++) {
+            cell = data[i + '_' + headers[j]];
+            if (cell == undefined) cell = '';
+            line += textQualifier + cell + textQualifier + deliminator;
+        }
+
+        line = line.slice(0, -1);
+        print(line);
+    }
+}
+*/
